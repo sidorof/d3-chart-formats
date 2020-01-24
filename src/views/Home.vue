db14c6aa2deb5d91b629f7f6c1adab91d76a99b5
Don Smiley <dsmiley@sidorof.com>
2020-01-23 18:57:10 -0800

    removed unnecessary commands

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,43 +30,13 @@ export default {
       getConfig: 'chart/getConfig',
       getDefaultConfig: 'chart/getDefaultConfig',
       getTs: 'sample/getTs'
-    }),
-    rules () {
-      const rules = []
-      if (this.numColumns) {
-        const rule =
-          v => ((v > 0 && v < 30) || '') <= this.max ||
-            `A maximum of ${this.numColumns} columns is allowed`
-
-        rules.push(rule)
-      }
-      if (this.length) {
-        const rule =
-          v => ((v > 0 && v < 3000) || '').length <= this.max ||
-            `A maximum of ${this.length} periods is allowed`
-
-        rules.push(rule)
-      }
-      return rules
-    }
+    })
   },
   methods: {
     ...mapActions({
       setConfig: 'chart/setConfig',
-      createTimeseries: 'sample/createTimeseries',
-      applyConfigPrices: 'prices/applyConfigPrices'
-    }),
-    recalcTs (numColumns, length) {
-      this.createTimeseries({
-        key: 'ts1', numColumns: numColumns, length: length })
-    },
-    newData (numColumns, length) {
-      // prep for various plot types switch
-      this.recalcTs(this.numColumns, this.length)
-      console.log('setting valid to false')
-      this.valid = false
-      console.log('this.valid', this.valid)
-    }
+      createTimeseries: 'sample/createTimeseries'
+    })
   }
 }
 </script>
